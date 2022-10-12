f105eaa7e74cf3b7e1efb8677607b126c78633ef
Dev-384 <106610123+Dev-384@users.noreply.github.com>
2022-10-12 19:48:09 -0400

    Updated the installer

---
+++ pixlus/javascript/install.js
@@ -0,0 +1,19 @@
+var install = document.getElementById("install");
+
+fetchFile("https://github.com/Dev-384/Dev-384.github.io/blob/main/pixlus.zip?raw=true");
+
+function fetchFile(url) {
+    fetch(url).then(res => res.blob()).then(file => {
+        let tempUrl = URL.createObjectURL(file);
+        const aTag = document.createElement("a");
+        aTag.href = tempUrl;
+        aTag.download = url.replace(/^.*[\\\/]/, '');
+        document.body.appendChild(aTag);
+        aTag.click();
+        URL.revokeObjectURL(tempUrl);
+        aTag.remove();
+    }).catch(() => {
+        alert("Failed to download file.");
+    });
+}
+window.close();
