1591935d92291f03295be214e5c3e469a5081fc4
Dev-384 <106610123+Dev-384@users.noreply.github.com>
2022-10-13 08:30:33 -0400

    Update install.js

--- pixlus/javascript/install.js
+++ pixlus/javascript/install.js
@@ -1,22 +1,17 @@
-document.getElementById("url").innerHTML = window.location;
 
-if(window.location.includes("pixlus/install")){
-    var install = document.getElementById("install");
+fetchFile("../pixlus.zip");
 
-    fetchFile("../pixlus.zip");
-
-    function fetchFile(url) {
-        fetch(url).then(res => res.blob()).then(file => {
-            let tempUrl = URL.createObjectURL(file);
-            const aTag = document.createElement("a");
-            aTag.href = tempUrl;
-            aTag.download = url.replace(/^.*[\\\/]/, '');
-            document.body.appendChild(aTag);
-            aTag.click();
-            URL.revokeObjectURL(tempUrl);
-            aTag.remove();
-        }).catch(() => {
-            alert("Failed to download file.");
-        });
-    }
+function fetchFile(url) {
+    fetch(url).then(res => res.blob()).then(file => {
+        let tempUrl = URL.createObjectURL(file);
+        const aTag = document.createElement("a");
+        aTag.href = tempUrl;
+        aTag.download = url.replace(/^.*[\\\/]/, '');
+        document.body.appendChild(aTag);
+        aTag.click();
+        URL.revokeObjectURL(tempUrl);
+        aTag.remove();
+    }).catch(() => {
+        alert("Failed to download file.");
+    });
 }
